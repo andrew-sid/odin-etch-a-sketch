151f4921d17c727a3f0fa38537d51276fa5a7d65
Andrey Sidorkin <101244727+andrew-sid@users.noreply.github.com>
2024-06-07 18:11:00 +0700

    Add change squares count functional

--- js/scipts.js
+++ js/scipts.js
@@ -1,9 +1,9 @@
-function main() {
-  const pad = document.querySelector(".pad");
-
-  for (let i = 0; i < 256; i++) {
+function renderSquares(pad, squares = 16) {
+  for (let i = 0; i < Math.pow(squares, 2); i++) {
     const square = document.createElement("div");
     square.classList.add("square");
+    square.style.width = 600 / squares - 2 + 'px'; // 600 - WIDTH of pad in css
+    square.style.height = 600 / squares - 2 + 'px'; // 600 - HEIGHT of pad in css
 
     square.addEventListener('mouseover', () =>  {
       square.style.backgroundColor = 'black';
@@ -11,6 +11,22 @@ function main() {
 
     pad.appendChild(square);
   }
+}
+
+function main() {
+  const pad = document.querySelector(".pad");
+
+  renderSquares(pad); //first render with default 16x16 squares
+
+  const squaresBtn = document.querySelector('.squaresBtn');
+  let userSize;
+
+  squaresBtn.addEventListener('click', () => {
+    userSize = +prompt('Enter your size(100 max): ');
+    pad.innerHTML = ''; // clear pad before rendering new squares
+    renderSquares(pad, userSize);
+  })
+
 }
 
 main();
