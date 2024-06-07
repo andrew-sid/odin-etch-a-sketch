83be68a09d558607f7b0ea63d2b1afcf18f2127f
Andrey Sidorkin <101244727+andrew-sid@users.noreply.github.com>
2024-06-07 18:34:34 +0700

    Add user input handler
    User input handler provides user from input wrong value and adding
    feature to cancel input

--- js/scipts.js
+++ js/scipts.js
@@ -2,14 +2,29 @@ function renderSquares(pad, squares = 16) {
   for (let i = 0; i < Math.pow(squares, 2); i++) {
     const square = document.createElement("div");
     square.classList.add("square");
-    square.style.width = 600 / squares - 2 + 'px'; // 600 - WIDTH of pad in css
-    square.style.height = 600 / squares - 2 + 'px'; // 600 - HEIGHT of pad in css
+    square.style.width = 600 / squares - 2 + "px"; // 600 - WIDTH of pad in css
+    square.style.height = 600 / squares - 2 + "px"; // 600 - HEIGHT of pad in css
 
-    square.addEventListener('mouseover', () =>  {
-      square.style.backgroundColor = 'black';
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
     });
 
     pad.appendChild(square);
+  }
+}
+
+function getUserSize() {
+  let userSize;
+  while (true) {
+    userSize = prompt("Enter your size(100 max): ");
+
+    if (userSize === null) return null;
+
+    if (userSize > 0 && userSize <= 100) {
+      return userSize;
+    } else {
+      alert("Please, enter number between 1 and 100");
+    }
   }
 }
 
@@ -18,15 +33,18 @@ function main() {
 
   renderSquares(pad); //first render with default 16x16 squares
 
-  const squaresBtn = document.querySelector('.squaresBtn');
+  const squaresBtn = document.querySelector(".squaresBtn");
   let userSize;
 
-  squaresBtn.addEventListener('click', () => {
-    userSize = +prompt('Enter your size(100 max): ');
-    pad.innerHTML = ''; // clear pad before rendering new squares
-    renderSquares(pad, userSize);
-  })
+  squaresBtn.addEventListener("click", () => {
+    userSize = getUserSize();
 
+    if (userSize) {
+      // if user cancel input, not rendering
+      pad.innerHTML = ""; // clear pad before rendering new squares
+      renderSquares(pad, userSize);
+    }
+  });
 }
 
 main();
