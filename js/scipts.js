5fa7484d839659c86592869180b95abfc29c33e8
Andrey Sidorkin <101244727+andrew-sid@users.noreply.github.com>
2024-06-07 17:23:06 +0700

    Add 16x16 grid

---
+++ js/scipts.js
@@ -0,0 +1,12 @@
+function main() {
+  const container = document.querySelector(".container");
+
+  for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+
+    container.appendChild(square);
+  }
+}
+
+main();
